5528fc8ac992914ee3274c228773634f7ba1fe2a
Cian Bagshaw <cian@cianb.xyz>
2026-09-25 02:54:30 +0100

    Tidied build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Groff command to run
-cmd() { groff -Tpdf cv.tmac $@ > cv.pdf; }
+cmd() { groff -Tpdf cv.tmac $@ cv.groff > cv.pdf; }
 
-# Only attempt to use 'info.groff' file if it is present - it may not be as it
-# is "gitignored". This should avoid errors. Provide a non-file argument to
+# Only attempt to use 'info.groff' file if it is present; it may not be as it
+# is 'gitignored'. This should avoid errors. Provide a non-file argument to
 # generate a redacted version.
 INFO_FILE=${1:-info.groff}
-if [ -f "$INFO_FILE" ]
-    then cmd "$INFO_FILE" cv.groff
-    else cmd cv.groff
-fi
+[ -f "$INFO_FILE" ] && cmd "$INFO_FILE" || cmd
